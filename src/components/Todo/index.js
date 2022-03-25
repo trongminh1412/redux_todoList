de9906d3b855ecdcb68585f86f9534f544994aad
Minh <nguyentrongminh1412@gmail.com>
2022-03-25 16:49:23 +0700

    update Todo

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,5 +1,7 @@
 import { Row, Tag, Checkbox } from 'antd';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { todoStatusComplete } from '../../redux/action';
 
 const priorityColorMapping = {
   High: 'red',
@@ -7,11 +9,13 @@ const priorityColorMapping = {
   Low: 'gray',
 };
 
-export default function Todo({ name, prioriry, completed }) {
+export default function Todo({ name, priority, completed, id }) {
   const [checked, setChecked] = useState(completed);
+  const dispatch = useDispatch();
 
   const toggleCheckbox = () => {
     setChecked(!checked);
+    dispatch(todoStatusComplete(id));
   };
 
   return (
@@ -25,8 +29,8 @@ export default function Todo({ name, prioriry, completed }) {
       <Checkbox checked={checked} onChange={toggleCheckbox}>
         {name}
       </Checkbox>
-      <Tag color={priorityColorMapping[prioriry]} style={{ margin: 0 }}>
-        {prioriry}
+      <Tag color={priorityColorMapping[priority]} style={{ margin: 0 }}>
+        {priority}
       </Tag>
     </Row>
   );
