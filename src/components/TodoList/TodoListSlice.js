72a644d0adde05e59b45da330072d9d9752b1212
Minh <nguyentrongminh1412@gmail.com>
2022-03-26 23:02:33 +0700

    update todo list

--- src/components/TodoList/TodoListSlice.js
+++ src/components/TodoList/TodoListSlice.js
@@ -1,27 +1,53 @@
-const initState = [
-  { id: 1, name: 'react', complete: false, priority: 'Medium' },
-  { id: 2, name: 'redux', complete: true, priority: 'High' },
-  { id: 3, name: 'next', complete: false, priority: 'Low' },
-];
+// const initState = [
+//   { id: 1, name: 'react', complete: false, priority: 'Medium' },
+//   { id: 2, name: 'redux', complete: true, priority: 'High' },
+//   { id: 3, name: 'next', complete: false, priority: 'Low' },
+// ];
 
-const todoListReducer = (state = initState, action) => {
-  //   console.log({ state, action });
-  switch (action.type) {
-    case 'todoList/addTodo':
-      return [...state, action.payload];
-    case 'todoList/todoStatusComplete':
-      return state.map((todo) =>
-        todo.id === action.payload
-          ? { ...todo, completed: !todo.complete }
-          : todo
-      );
-    default:
-      return state;
-  }
-};
+// const todoListReducer = (state = initState, action) => {
+//   //   console.log({ state, action });
+//   switch (action.type) {
+//     case 'todoList/addTodo':
+//       return [...state, action.payload];
+//     case 'todoList/todoStatusComplete':
+//       return state.map((todo) =>
+//         todo.id === action.payload
+//           ? { ...todo, completed: !todo.complete }
+//           : todo
+//       );
+//     default:
+//       return state;
+//   }
+// };
 
-export default todoListReducer;
+// export default todoListReducer;
+// /**
+//  * làm việc với reducer nên chia nhỏ tính năng , dễ quản lý hơn
+//  * ta tách các chức năng trong reducer trực tiêp vô component
+//  */
+
 /**
- * làm việc với reducer nên chia nhỏ tính năng , dễ quản lý hơn
- * ta tách các chức năng trong reducer trực tiêp vô component
+ * REDUX TOOLKIT
  */
+
+import { createSlice } from '@reduxjs/toolkit';
+
+export default createSlice({
+  name: 'todoList',
+  initialState: [
+    { id: 1, name: 'react', complete: false, priority: 'Medium' },
+    { id: 2, name: 'redux', complete: true, priority: 'High' },
+    { id: 3, name: 'next', complete: false, priority: 'Low' },
+  ],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+    todoStatusComplete: (state, action) => {
+      const currentTodo = state.find((todo) => todo.id === action.payload);
+      if (currentTodo) {
+        currentTodo.completed = !currentTodo.complete;
+      }
+    },
+  },
+});
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,11 @@
 import { Col, Row, Input, Button, Select, Tag } from 'antd';
 import Todo from '../Todo';
 import { useDispatch, useSelector } from 'react-redux';
-import { addTodo } from '../../redux/action';
+// import { addTodo } from '../../redux/action';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import { todoRemaining } from '../../redux/selectors';
+import todoListSlice from './todoListSlice';
 
 export default function TodoList() {
   const [todoName, setTodoName] = useState('');
@@ -27,7 +28,7 @@ export default function TodoList() {
 
   const handleAddButton = () => {
     dispatch(
-      addTodo({
+      todoListSlice.actions.addTodo({
         id: uuidv4(),
         name: todoName,
         completed: false,
